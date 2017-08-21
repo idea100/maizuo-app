c0bf594941c69da45fe530606b6f5838b02c4421
huxiang <517626980@qq.com>
2017-08-22 00:40:39 +0800

    add fetch

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Header from './header'
 import Content from './content'
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
--- src/components/content.js
+++ src/components/content.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react'
+import { fetchImages } from '@/service/getData'
+
 
 export default class Content extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      images: []
+    }
+  }
+
+  componentDidMount () {
+    fetchImages().then(resp => this.setState({images: resp.data.billboards}))
+  }
+
   render () {
     return (
       <div className="slide">
         <div>
           <ul>
-            <li className="silder1">
-              <img src="https://static.maizuo.com/pc/v5/h5PicUpload/9c6b27260a23a248a49548979779168f.jpg" alt=""/>
-            </li>
-
-            <li className="silder2">
-              <img src="https://static.maizuo.com/pc/v5/h5PicUpload/8c6e50d09e8fb3b20386655753a0fdf2.jpg" alt=""/>
-            </li>
-
-            <li className="silder3">
-              <img src="https://static.maizuo.com/pc/v5/h5PicUpload/af62452085bb3a5ff275feb89c33e134.jpg" alt=""/>
-            </li>
+            {
+              this.state.images.map((item, index) =>(
+                <li className={'silder' + index} key={index}>
+                  <img src={item.imageUrl} alt={item.name}/>
+                </li>
+              ))
+            }
           </ul>
         </div>
       </div>
