d6a4fb822d683e4d8e4df3520869ff3bd1eac3ee
huxiang <517626980@qq.com>
2017-09-18 00:43:06 +0800

    read cityName by cookie

---
+++ src/config/utils.js
@@ -0,0 +1,6 @@
+export const getCookie = name => {
+  let match = document.cookie.match(new RegExp(name + '=([^;]+)'))
+  if (match) {
+    return decodeURI(match[1])
+  }
+}
--- src/container/header.js
+++ src/container/header.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 import { AlignJustify, User } from 'react-feather'
+import { getCookie } from '@/config/utils'
 
 export default class Header extends Component {
+
   render () {
+    let cityName = getCookie('cityName')
+
     return (
       <header className="header">
         <AlignJustify className="menu-icon" onClick={() => alert('you click menu')}/>
         <span className="maizuo-title">{this.props.title}</span>
         <User className="maizuo-user" onClick={ () => this.props.history.push('/login') } />
-        <span className="maizuo-city" onClick={ () => this.props.history.push('/citys') }>深圳</span>
+        <span className="maizuo-city" onClick={ () => this.props.history.push('/citys') }>{ cityName }</span>
       </header>
     )
   }
