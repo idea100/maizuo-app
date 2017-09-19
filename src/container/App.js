d56b289be51082d29f81c1f93acaef04358ea9f5
huxiang <517626980@qq.com>
2017-09-19 10:02:10 +0800

    clean code

--- src/container/App.js
+++ src/container/App.js
@@ -4,21 +4,42 @@ import City from '@/pages/citys'
 import Login from '@/pages/login'
 
 import {
-  BrowserRouter as Router,
+  Router,
   Route
-} from 'react-router-dom'
+} from 'react-router'
+
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+import reducers from '@/reducers'
+import createHistory from 'history/createBrowserHistory'
+
+const middleware = [ thunk ]
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger())
+}
+
+const store = createStore(
+  reducers,
+  applyMiddleware(...middleware)
+)
+
+const history = createHistory()
 
 class App extends Component {
 
   render() {
     return (
-      <Router>
-        <div>
-          <Route exact path="/" component={ Content }></Route>
-          <Route path="/citys" component={ City }></Route>
-          <Route path="/login" component={ Login }></Route>
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router history={history}>
+          <div>
+            <Route exact path="/" component={ Content }></Route>
+            <Route path="/citys" component={ City }></Route>
+            <Route path="/login" component={ Login }></Route>
+          </div>
+        </Router>
+      </Provider>
     )
   }
 }
--- src/main.js
+++ src/main.js
@@ -3,25 +3,9 @@ import { render } from 'react-dom'
 import App from './container/App'
 import './style/common.scss'
 
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-import { createLogger } from 'redux-logger'
-import reducer from '@/reducers'
 
-const middleware = [ thunk ]
-if (process.env.NODE_ENV !== 'production') {
-  middleware.push(createLogger())
-}
-
-const store = createStore(
-  reducer,
-  applyMiddleware(...middleware)
-)
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <App />,
   document.getElementById('root')
 )
