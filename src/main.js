010bc991d7e22ae071656674fe322671777bec53
huxiang <517626980@qq.com>
2017-10-10 18:12:12 +0800

    add icon

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './container/App'
 import './style/common.scss'
-
-
+import '../static/css/icon.css'
 
 render(
   <App />,
